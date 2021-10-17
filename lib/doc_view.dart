1e088b6e783e57bdfd6f5bad64f860a9b8485d64
SanskarKashyap <72290416+SanskarKashyap@users.noreply.github.com>
2021-10-17 16:47:38 +0530

    Update doc_view.dart

--- lib/doc_view.dart
+++ lib/doc_view.dart
@@ -7,25 +7,29 @@ class DocView {
   static const String _getImage = "getImage";
   static const String _genThumbnail = "genThumbs";
   static const String _fetchText = "fetchText";
-
-  static Future<int> pageCount(String path) async {
+  
+  // count pages from the pdf 
+ static Future<int> pageCount(String path) async {
     final int _pageCount = await _channel
         .invokeMethod(DocView._pageCount, <String, dynamic>{'path': path});
     return _pageCount;
   }
 
+  // generate image of a selleced page from the PDF
   static Future<dynamic> getImage(String path, int index) async {
     final _pageCount = await _channel.invokeMethod(
         DocView._getImage, <String, dynamic>{'path': path, 'index': index});
     return _pageCount;
   }
-
+  
+  // generate thumbnail of the PDF 
   static Future<Object> genThumbnail(String path) async {
     final result = await _channel
         .invokeMethod(DocView._genThumbnail, <String, dynamic>{'path': path});
     return result;
   }
 
+  // fetch text from the selected page(INDEX) of the PDF 
   static Future<String> fetchText(String path, int index) async {
     final String result = await _channel.invokeMethod(
         DocView._fetchText, <String, dynamic>{'path': path, 'index': index});
